12be9eeea0a1512cbc4cae22496b4fb30db1cf01
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2024-04-17 22:40:34 +0300

    test: added intgrity test

--- test/client_conformance_test.dart
+++ test/client_conformance_test.dart
@@ -2,11 +2,37 @@
 library;
 
 import 'package:fetch_client/fetch_client.dart';
+import 'package:http/http.dart';
 import 'package:http_client_conformance_tests/http_client_conformance_tests.dart';
 import 'package:test/test.dart';
 
 
 void main() {
+  group('integrity tests', () {
+    final client = FetchClient(mode: RequestMode.cors);
+    final uri = Uri.parse('https://raw.githubusercontent.com/Zekfad/fetch_client/22c3a2732c4a89ef284827cba4a7e62a01535776/LICENSE');
+
+    test('throw error when integrity mismatch', () async {
+      const integrity = 'sha256-0';
+      final request = FetchRequest(
+        Request('GET', uri),
+      )..integrity = integrity;
+      await expectLater(
+        client.send(request),
+        throwsA(isA<ClientException>()),
+      );
+    });
+
+    test('succeed with correct integrity', () async {
+      const integrity = 'sha256-NTaW0fWGbetqbg/iB0CfyvrxlEvm4rk3f1MXq+Zu0S8=';
+      final request = FetchRequest(
+        Request('GET', uri),
+      )..integrity = integrity;
+      final response = await client.send(request);
+      expect(response.statusCode, 200);
+    });
+  });
+
   group('client conformance tests', () {
     testAll(
       () => FetchClient(mode: RequestMode.cors),
