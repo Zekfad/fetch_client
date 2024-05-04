f4f5e6e19eb150b3a9651eceb376f6b5f6a77f44
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2024-05-04 20:02:03 +0300

    test: add vm shim test

---
+++ test/vm_shim_test.dart
@@ -0,0 +1,18 @@
+@TestOn('!js')
+library;
+
+import 'package:fetch_client/fetch_client.dart';
+import 'package:test/test.dart';
+
+
+void main() {
+  group('vm shim test', () {
+    test('doesn\'t prevent import in vm environment', () {
+      expect(RequestMode.cors.toString(), equals('cors'));
+    });
+
+    test('throws unsupported error in vm', () {
+      expect(FetchClient.new, anyOf(throwsUnsupportedError));
+    });
+  });
+}
