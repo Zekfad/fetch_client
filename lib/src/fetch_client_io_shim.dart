e0275df8ae8781620ca3a4eda60e490ff9f20b70
Yaroslav Vorobev <vorobevyaroslav@gmail.com>
2025-03-09 21:18:27 +0300

    docs: remove docs for io shim

--- lib/src/fetch_client_io_shim.dart
+++ lib/src/fetch_client_io_shim.dart
@@ -1,5 +1,6 @@
 /// This shim library mimics FetchClient to allow imports from non-JS platforms.
 /// @nodoc
+// ignore: unnecessary_library_name
 library fetch_client_io_shim;
 
 import 'package:fetch_api/enums.dart';
@@ -8,7 +9,9 @@ import 'fetch_response.dart';
 import 'redirect_policy.dart';
 
 
+/// @nodoc
 class FetchClient extends BaseClient {
+  /// @nodoc
   FetchClient({
     this.mode = RequestMode.noCors,
     this.credentials = RequestCredentials.sameOrigin,
@@ -21,12 +24,25 @@ class FetchClient extends BaseClient {
     throw UnsupportedError('Unsupported platform');
   }
 
+  /// @nodoc
   final RequestMode mode;
+
+  /// @nodoc
   final RequestCredentials credentials;
+
+  /// @nodoc
   final RequestCache cache;
+
+  /// @nodoc
   final String referrer;
+
+  /// @nodoc
   final RequestReferrerPolicy referrerPolicy;
+
+  /// @nodoc
   final RedirectPolicy redirectPolicy;
+
+  /// @nodoc
   final bool streamRequests;
 
   @override
